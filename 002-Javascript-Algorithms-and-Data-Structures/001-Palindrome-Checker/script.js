61e6cbaa78ccacd7e73ee029c2ced57019df4b86
smulbero <smulbero@gmail.com>
2025-03-15 12:03:47 +0200

    Add few notes to script.js

--- 002-Javascript-Algorithms-and-Data-Structures/001-Palindrome-Checker/script.js
+++ 002-Javascript-Algorithms-and-Data-Structures/001-Palindrome-Checker/script.js
@@ -17,15 +17,17 @@ const isPalindrome = (str) => {
   /*
   Match any character that is not a letter,  digit, or underscore. 
   The underscore _ is explicitly included in the character class to ensure it is matched.
+  Clean the string with regex and convert it to lowercase
   */
   const regex = /[\W_]/g;
-  const cleanStr = str.replace(regex, '').toLowerCase();  
+  const cleanStr = str.replace(regex, '').toLowerCase();
+  // Split the string into an array of characters, reverse the array, and then join the array back into a string  
   const reversedStr = cleanStr.split('').reverse().join('');
 
-  console.log(`
-    Clean String: ${cleanStr}\n
-    Reversed String: ${reversedStr}
-  `)
+  // console.log(`
+  //   Clean String: ${cleanStr}\n
+  //   Reversed String: ${reversedStr}
+  // `)
 
   if (cleanStr === reversedStr) {
     return `${str} is a palindrome`;
