648f4b88a86dc4d7dde55c76bab74ed9c7d84515
smulbero <smulbero@gmail.com>
2025-07-20 22:16:57 +0300

    Removed unnecessary instruction.mds and created readme.md

--- 002-Javascript-Algorithms-and-Data-Structures/002-Roman-Numeral-Converter/script.js
+++ 002-Javascript-Algorithms-and-Data-Structures/002-Roman-Numeral-Converter/script.js
@@ -19,23 +19,6 @@ const ROMAN_NUMERALS = {
   1: "I"
 }
 
-// Useless variable
-// const ARABIC_NUMERALS = {
-//   "M": 1000,
-//   "CM": 900,
-//   "D": 500,
-//   "CD": 400,
-//   "C": 100,
-//   "XC": 90,
-//   "L": 50,
-//   "XL": 40,
-//   "X": 10,
-//   "IX": 9,
-//   "V": 5,
-//   "IV": 4,
-//   "I": 1
-// }
-
 CONVERTBTN.addEventListener("click", (e) => {  
   e.preventDefault();  
   const USERINPUTVAL = parseInt(USERINPUT.value);
